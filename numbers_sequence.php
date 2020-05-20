e990d516d854f51b03d16c0a967e8287d3e051a0
Stanislav Gnoyanko <gsa038@gmail.com>
2020-05-20 23:18:59 +0300

    task-1-7

--- numbers_sequence.php
+++ numbers_sequence.php
@@ -2,37 +2,11 @@
 
 declare(strict_types=1);
 
-function getNumsFromConsole(): array {
-    echo "Enter a sequence of numbers in format: 1,2,3..etc.:\n";
-    $numSeqString = readline();
-    $numSeqArray = explode(",", $numSeqString);
-    foreach ($numSeqArray as $value) {
-        if (!is_numeric($value)){
-            fwrite(STDERR, "'$value' is not a number.\nAll must be a number!\n");
-            exit();
-        }
-        // first variant input float cheching is using
-        // Second variant check ($f == (string)(float)$f)
-        elseif (is_float($value + 0)){ 
-            fwrite(STDERR,"'$value' is a float!\nAll must be integer");
-            exit();
-        } 
-        elseif ((!(int) $value > 0)) {
-            fwrite(STDERR,"'$value' is smaller than 1.\nAll integer numbers must be greater than 0");
-            exit();
-        }
-    }
-    return array_map('intval', $numSeqArray);
-}
-
 $numSeq = getNumsFromConsole();
 
-echo "Choose an action:\n 
-    1 - Get reverse order sorted sequence\n
-    2 - Get ascending sorted sequence.\n 
-    3 - Get descending sorted sequence\n";
+// $userAction = useMenuAction();
+$userAction = useArgsAction();
 
-$userAction = readline();
 switch ($userAction) {
     case "1":
         krsort($numSeq);
@@ -43,13 +17,77 @@ switch ($userAction) {
     case "3":
         arsort($numSeq);
         break;
+    case "4":
+        getNumsPercent($numSeq);
+        break;
     default:
-        echo "Wrong input!";
-        exit();
+        echo "Wrong input!\nWill be Returned non-changed array\n";
 }
 
-$numSeqPercent = array_sum($numSeq) / 100;
 foreach ($numSeq as $value) {
-    $valuePercent = round($value / $numSeqPercent, 2) ;
-    echo "$value - $valuePercent%\n";
+    echo "$value\n";
+}
+
+function getUserInput(string $text): string {
+    echo "$text\n";
+    return readline();
+}
+
+function getNumsFromConsole(): array {
+    $numSeqString = getUserInput("Enter a sequence of numbers in format: 1,2,3..etc.:");
+    $numSeqArray = explode(",", $numSeqString);
+    foreach ($numSeqArray as $value) {
+        if (!is_numeric($value)){
+            fwrite(STDERR, "'$value' is not a number.\nAll must be a number!\n");
+            exit();
+        }
+        if ((string)(int)$value != $value) { 
+            fwrite(STDERR,"'$value' is a float!\nAll must be integer");
+            exit();
+        } 
+        if ((!(int) $value > 0)) {
+            fwrite(STDERR,"'$value' is smaller than 1.\nAll integer numbers must be greater than 0");
+            exit();
+        }
+        $value = (int) $value;
+    }
+    return $numSeqArray;
+}
+
+function getNumsPercent(&$array): array {
+    $valueSeqPercent = array_sum($array) / 100;
+    foreach ($array as &$value) {
+        $valuePercent = round($value / $valueSeqPercent, 2);
+        $value = "$value - $valuePercent%\n";
+    }
+    unset($value);
+    return $array;
+}
+
+function useMenuAction(): string {
+    echo "Choose an action:\n 
+        1 - Get reverse order sorted sequence\n
+        2 - Get ascending sorted sequence.\n 
+        3 - Get descending sorted sequence\n
+        4 - Get percent of numbers of sequence\n";
+    return $userAction = readline();
+}
+
+function useArgsAction(): string {
+    if ($argv['-p']){
+        return '4';
+    }
+    if ($argv['--order']) {
+        switch ($argv['--order']) {
+            case 'reverse':
+                return '1';
+            case 'asc':
+                return '2';
+            case 'desc':
+                return '3';
+            default:
+                fwrite(STDERR, "Unknoun parameter of argumrent was used");
+        }
+    fwrite(STDERR, "Unknoun argumrent was used");
+    }
 }

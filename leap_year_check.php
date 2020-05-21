9d30a391f97da0fd793882eea8cc882b7ddf7169
Stanislav Gnoyanko <gsa038@gmail.com>
2020-05-21 21:13:53 +0300

    Task-1-4_leap_year_check

---
+++ leap_year_check.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+if ($argc > 2) {
+    echo "1 additional aggument expected";
+    exit();
+}
+
+if ($argc == 1) {
+    $year = getUserInput("Enter year as integer number");
+}
+
+if ($argc == 2) {
+    $year = $argv[1];
+}
+
+if (strlen($year) != 4 || !ctype_digit($year)) {
+    fwrite(STDERR, "Year must consist 4 positive integer");
+    exit();
+}
+
+fwrite(STDOUT, isLeapYear((int)$year));
+
+function isLeapYear(int $year):string {
+    $date = strtotime("$year-01-01");
+    if (date('L',$date)) {
+        return "True";
+    }
+    return "False";
+}
+
+function getUserInput(string $text):string {
+    echo "$text:\n";
+    return readline();
+}

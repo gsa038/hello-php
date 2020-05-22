c8d3bd48c2fc9b11baac023b70bd3169d634be49
gsa038@ya.ru <gsa038@ya.ru>
2020-05-22 16:11:59 +0300

    upd

--- leap_year_check.php
+++ leap_year_check.php
@@ -17,17 +17,18 @@ if ($argc == 2) {
     $year = $argv[1];
 }
 
-if (strlen($year) != 4 || !ctype_digit($year)) {
-    fwrite(STDERR, "Year must consist 4 positive integer");
+if (strlen($year) > 4 || !ctype_digit($year)) {
+    fwrite(STDERR, "Year must consist max 4 positive integer");
     exit();
 }
 
-fwrite(STDOUT, isLeapYear((int)$year));
 
-function isLeapYear(int $year):string {
+fwrite(STDOUT, printBool(isLeapYear((int)$year)));
+
+function isLeapYear(int $year): bool {
     $date = strtotime("$year-01-01");
     if (date('L',$date)) {
-        return "True";
+        return true;
     }
-    return "False";
+    return false;
 }
--- utils.php
+++ utils.php
@@ -6,3 +6,10 @@ function getUserInput(string $text):string {
     echo "$text:\n";
     return readline();
 }
+
+function printBool(bool $bool): string {
+    if ($bool == 1) {
+        return "True";
+    }
+    return "False";
+}

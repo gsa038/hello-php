7ef81cb6937632485f5ce1e4879b1076a2cd8ea7
Stanislav Gnoyanko <gsa038@gmail.com>
2020-05-26 21:54:09 +0300

    task-2-1

---
+++ print_string_to_array.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+require 'utils.php';
+
+$stringToConvert = getUserInput('Input string kind of "a=1;b=2; c=agfda; derp=; eee="');
+
+var_dump(stringToArray($stringToConvert));
+
+function stringToArray(string $stringToConvert): array
+{
+    $parts = explode(';', $stringToConvert);
+    $parts = array_map('trim', $parts);
+    foreach ($parts as $part) {
+        list($key, $value) = explode('=', array_values($part));
+        $keyValueArray[$key] = $value;
+    }
+    return $keyValueArray;
+}

d5e4fc5de55c9d97cb17e3fae59e349efcc7ed5b
Stanislav Gnoyanko <gsa038@gmail.com>
2020-05-27 23:02:46 +0300

    upd

--- from_decimal_converter.php
+++ from_decimal_converter.php
@@ -4,24 +4,22 @@ declare(strict_types=1);
 
 require 'utils.php';
 
-$numbersSystem = getUserInput("Please input numbers system as integer number in range 2-16");
+$numbersSystem = getUserInput("Input numbers system as integer number in range 2-16");
 
 if (!ctype_digit($numbersSystem) || (2 > (int)$numbersSystem) || ((int)$numbersSystem > 16)) {
-    fwrite(STDERR, "You need to input integer number in range 2-16 as numbers system!");
+    printError("You need to input integer number in range 2-16 as numbers system!");
     exit();
 }
 
-$decimalNumbersSequence = explode("," , getUserInput("Please input a sequence of decumal number looks as 1,2,3,4,5..etc"));
+$decimalNumbersSequence = explode("," , getUserInput("Input a sequence of decimal numbers in format: 1,2,3,4,5..etc"));
 
 // check only.. For check result perfomance
 foreach($decimalNumbersSequence as $number) {
     if (!ctype_digit($number)) {
-        fwrite(STDERR, "$number isn't a positive decimal integer number");
+        printError("$number isn't a positive decimal integer number");
         exit();
     }
 }
-
-$hexSymbols = ['10' => 'A', '11' => 'B', '12' => 'C', '13' => 'D', '14' => 'E', '15' => 'F'];
 
 $numbersSystem =(int)$numbersSystem;
 
@@ -30,27 +28,35 @@ $numbersSystem > 10 ? $useHex = true : $useHex = false;
 
 $newNumbersArray = [];
 foreach($decimalNumbersSequence as $number) {
+    $newNumbersArray[$number] = getInNewBase($numbersSystem, $number, $useHex);
+}
+
+printInfo("Numbers system is $numbersSystem\n");
+foreach($newNumbersArray as $oldNumber => $newNumber) {
+    printInfo("$oldNumber = $newNumber\n");
+}
+
+function getInNewBase(int $base, string $number, bool $useHex): string
+{
+    $hexSymbols = ['10' => 'A', '11' => 'B', '12' => 'C', '13' => 'D', '14' => 'E', '15' => 'F'];
     $number = (int)$number;
     $reminder = 0;
     $newNumberString = '';
-    do {
+    while (true) {
         if ($reminder === 0) {
             $reminder = $number;
         }
-        $part = $reminder % $numbersSystem;
-        $reminder = intdiv($reminder, $numbersSystem);
+        $part = $reminder % $base;
+        $reminder = intdiv($reminder, $base);
         if ($useHex && (int)$part > 10) {
+
             $part = $hexSymbols["$part"];
         } else {
             $part = "$part";
         }
         $newNumberString = $part . $newNumberString;
+        if ($reminder === 0) {
+            return $newNumberString;
+        }
     }
-    while ($reminder !== 0);
-    $newNumbersArray[$number] = $newNumberString;
-}
-
-fwrite(STDOUT, "Numbers system is $numbersSystem\n");
-foreach($newNumbersArray as $oldNumber => $newNumber) {
-    fwrite(STDOUT, "$oldNumber = $newNumber\n");
 }

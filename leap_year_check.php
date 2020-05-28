c5e99efa3ff073a780d0cba4cb03a445ed6fe3a1
gsa038@ya.ru <gsa038@ya.ru>
2020-05-28 10:42:17 +0300

    upd

--- leap_year_check.php
+++ leap_year_check.php
@@ -24,35 +24,35 @@ if (!$isValidYear) {
     exit();
 }
 
-$isLeapYear = getIsLeapYearString((int)$year);
-$responceText = printIsLeap($isLeapYear, $year);
+$isLeapYear = getIsLeapYear((int)$year);
+$responceText = getIsLeapString($isLeapYear, $year);
 printInfo($responceText);
 
-function getIsLeapYearString(int $year): bool
+function getIsLeapYear(int $year): bool
 {
     // Annalistic leap years from 45 before A.D. uo to 12 after A.D.
     $fixedLeapYears = [-42, -39, -36, -33, -30, -27, -24, -21, -18, -15, -12, -9, 8, 12];
     if ($year >= -45 && $year <= 12) {
         return in_array($year, $fixedLeapYears);
     }
-    if ($year > 12) {
-        // Leap years after reform of Gregory XIII in 1582 after A.D.
-        if ($year > 1582) {
-            if ($year % 400 === 0) {
-                return true;
-            } elseif ($year % 100 === 0) {
-                return false;
-            }
-        }
-        // Leap years between 12 and 1582 after A.D.
-        if ($year % 4 === 0) {
+    if (!($year > 12)) {
+        return false;
+    }
+    // Leap years after reform of Gregory XIII in 1582 after A.D.
+    if ($year > 1582) {
+        if ($year % 400 === 0) {
             return true;
+        } elseif ($year % 100 === 0) {
+            return false;
         }
     }
-    return false;
+    // Leap years between 12 and 1582 after A.D.
+    if ($year % 4 === 0) {
+        return true;
+    }
 }
 
-function printIsLeap(bool $isLeap, string $year): string
+function getIsLeapString(bool $isLeap, string $year): string
 {
     if ($isLeap) {
         return "The year $year is Leap";

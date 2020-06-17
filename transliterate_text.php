74aed178fbb832aee407f1da0f6b9860344ed5ac
Stanislav Gnoyanko <gsa@gsa.us.local>
2020-06-17 16:45:44 +0300

    task-2-5-draft

--- transliterate_text.php
+++ transliterate_text.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 require_once 'utils.php';
 
-$text = getUserInput("Enter text for transliteration:");
+$text = getUserInput("Enter text for transliteration");
 
-$trasliteratedText = transliterateString($text);
+if (!isCyrillicText($text)) {
+    printError("The sting must contain only cyrillic symbols!\n");
+    exit();
+}
 
-printInfo($trasliteratedText."\n");
+function isCyrillicText(string $text): bool {
+    for ($i = 0; $i < strlen($text); $i++) {
+        if (!preg_match("/[А-Яа-яЁё]/", $text[$i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$transliteratedText = transliterateString($text);
+
+printInfo($transliteratedText."\n");
 
 function transliterateString(string $text): string
 {
-    return transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $text);
+    return transliterator_transliterate('Russian-Latin/BGN', $text);
 }

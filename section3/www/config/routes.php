b0eeb9dc38c0cd7e901870d6abb293f97e8ec5e7
Stanislav A Gnoyanko <gsa038@mail.ru>
2020-08-12 12:17:26 +0300

    task-3-2 init commit

--- section3/www/config/routes.php
+++ section3/www/config/routes.php
@@ -6,4 +6,5 @@ return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
     $app->get('/hello/{yourName}', \App\Action\HomeAction::class);
     $app->get('/factorial/{number}', \App\Action\Factorial::class);
+    $app->get('/fibonacci/{number}', \App\Action\Fibonacci::class);
 };
---
+++ section3/www/src/Action/Fibonacci.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Fibonacci
+{
+    public function __invoke(
+        ServerRequestInterface $request, 
+        ResponseInterface $response
+    ): ResponseInterface {
+        if (is_numeric($request->getAttribute('number'))) {
+            $number = $request->getAttribute('number');
+            list($fibonacci, $counter) = $this->calcFibonacci($number);
+            $response->getBody()->write("$number $fibonacci");
+            return $response;
+        }
+        $response->getBody()->write('Bad request');
+        return $response;
+    }
+    
+    private function calcFibonacci(int $number): int
+    {
+        if ($number > 1) {
+            $fib = $this->calcFibonacci($number - 1) + $this->calcFibonacci($number - 2);
+            return $fib;
+        }
+        return 1;
+    }
+}
